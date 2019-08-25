1789b687bd78aedaa6676da00c9f34bf7f104a04
zhangzisheng <zhangzisheng@DESKTOP-8N96TCV>
2019-08-25 13:23:22 +0800

    test

--- src/helloword/HelloGitWorld.java
+++ src/helloword/HelloGitWorld.java
@@ -2,6 +2,6 @@ package helloword;
 
 public class HelloGitWorld {
 	public static void main(String[] args) {
-		System.out.println("hello git world!");
+		System.out.println("hello git world! change a little!");
 	}
 }
